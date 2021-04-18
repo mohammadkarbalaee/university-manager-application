b4704d89053e2e00deaee0dc2a64194656ff3913
Muhammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2021-04-18 18:16:59 +0430

    done and done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <vector>
 #include "string"
 using namespace std;
+
+const int CLASSES = 1;
+const int quantitySTUDENTS = 1;
 
 class Teacher
 {
@@ -230,7 +234,7 @@ void showClassData(SchoolClass aClass)
     cout << "teacher's name: " << aClass.teacher.getFirstName() << " " << aClass.teacher.getLastName() << endl;
     cout << "teacher's national ID: " << aClass.teacher.getNationalId() << endl;
     cout << "teacher's birthday: " << aClass.teacher.getBirthYear() << "/" << aClass.teacher.getBirthMonth() << "/" << aClass.teacher.getBirthDay() << endl;
-    for (int i = 0; i < 30; ++i)
+    for (int i = 0; i < quantitySTUDENTS; ++i)
     {
         cout << "student " << i + 1 << endl;
         cout << "teacher's name: " << aClass.students[i].getFirstName() << " " << aClass.students[i].getLastName() << endl;
@@ -244,7 +248,7 @@ void showAllData(School school)
     cout << "----------------\n";
     cout << "school's name: " << school.getName() << endl;
     cout << "school's id: " << school.getIdNumber() << endl;
-    for (int i = 0; i < 10; ++i)
+    for (int i = 0; i < CLASSES; ++i)
     {
         cout << "class number " << i + 1 << endl;
         showClassData(school.classes[i]);
@@ -264,152 +268,181 @@ int main()
            "1:setting the information for schools\n"
            "2:viewing the information entered\n"
            "3:show school's information by code\n"
-           "5:delete an school\n"
            "4:search teacher\n"
+           "5:delete an school\n"
            "6:search students\n"
            "7:showing teachers information working at a school\n"
            "8:show name and national id of students\n"
-           "9:edit info\n"
-           "10:show based on birth date\n"
-           "0:terminate\n"
+           "9:search by birth date\n"
+           "10:terminate\n"
            );
-    cin >> choice;
-    if (choice == 1)
+    while (choice != 10)
     {
-        for (int i = 0; i < schoolsQuantity; ++i)
+        cin >> choice;
+        if (choice == 1)
         {
-            cout << "school number " << i +1 << endl << endl;
-            schools[i] = setSchoolInformation();
-        }
-    }
-    else if (choice == 2)
-    {
-        for (int i = 0; i < schoolsQuantity; ++i)
-        {
-            schoolds[i] = schools[i].getIdNumber();
-        }
-        int key;
-        int properLocation;
-        for (int i = 1; i < schoolsQuantity; i++)
-        {
-            key = schoolds[i];
-            for (properLocation = i - 1;properLocation >= 0 && key < schoolds[properLocation];properLocation--)
+            for (int i = 0; i < schoolsQuantity; ++i)
             {
-                schoolds[properLocation + 1] = schoolds[properLocation];
+                cout << "school number " << i +1 << endl << endl;
+                schools[i] = setSchoolInformation();
             }
-            schoolds[properLocation + 1] = key;
         }
-        for (int i = 0; i < schoolsQuantity; ++i)
+        else if (choice == 2)
         {
+            for (int i = 0; i < schoolsQuantity; ++i)
+            {
+                schoolds[i] = schools[i].getIdNumber();
+            }
+            int key;
+            int properLocation;
+            for (int i = 1; i < schoolsQuantity; i++)
+            {
+                key = schoolds[i];
+                for (properLocation = i - 1;properLocation >= 0 && key < schoolds[properLocation];properLocation--)
+                {
+                    schoolds[properLocation + 1] = schoolds[properLocation];
+                }
+                schoolds[properLocation + 1] = key;
+            }
+            for (int i = 0; i < schoolsQuantity; ++i)
+            {
+                if (schoolds[i] == 0)
+                {
+                    continue;
+                }
+                for (int j = 0; j < schoolsQuantity; ++j)
+                {
+                    if (schoolds[i] == schools[j].getIdNumber())
+                    {
+                        showAllData(schools[j]);
+                    }
+                }
+            }
+        }
+        else if (choice == 3)
+        {
+            int id = 0;
+            cout << "enter the id of your wanted school\n";
+            cin  >> id;
             for (int j = 0; j < schoolsQuantity; ++j)
             {
-                if (schoolds[i] == schools[j].getIdNumber())
+                if (id == schools[j].getIdNumber())
                 {
                     showAllData(schools[j]);
                 }
             }
         }
-    }
-    else if (choice == 3)
-    {
-        int id = 0;
-        cout << "enter the id of your wanted school\n";
-        cin  >> id;
-        for (int j = 0; j < schoolsQuantity; ++j)
+        else if (choice == 5)
         {
-            if (id == schools[j].getIdNumber())
+            int id = 0;
+            cout << "enter the id of an school which you want to delete\n";
+            cin >> id;
+            int target = 0;
+            for (int i = 0; i < schoolsQuantity; ++i)
             {
-                showAllData(schools[j]);
-            }
-        }
-    }
-    else if (choice == 4)
-    {
-        int id = 0;
-        cout << "enter the id of an school which you want to delete\n";
-        cin >> id;
-        int target = 0;
-        for (int i = 0; i < schoolsQuantity; ++i)
-        {
-            if (id == schools[i].getIdNumber())
-            {
-                target = i;
-                break;
-            }
-        }
-        for (int i = target; i < schoolsQuantity - 1; ++i)
-        {
-            schools[i] = schools[i+1];
-        }
-    }
-    else if (choice == 5)
-    {
-        int id = 0;
-        cout << "enter the national id of the teacher\n";
-        cin >> id;
-        for (int i = 0; i < schoolsQuantity; ++i)
-        {
-            for (int j = 0; j < 10; ++j)
-            {
-                if (schools[i].classes[j].teacher.getNationalId() == id)
+                if (id == schools[i].getIdNumber())
                 {
-                    cout << schools[i].getName() << endl;
+                    target = i;
+                    break;
                 }
             }
+            schools[target].setIdNumber(0);
         }
-    }
-    else if (choice == 6)
-    {
-        int id = 0;
-        cout << "enter the national id of the student\n";
-        cin >> id;
-        for (int i = 0; i < schoolsQuantity; ++i)
+        else if (choice == 4)
         {
-            for (int j = 0; j < 10; ++j)
+            int id = 0;
+            cout << "enter the national id of the teacher\n";
+            cin >> id;
+            for (int i = 0; i < schoolsQuantity; ++i)
             {
-                for (int k = 0; k < 30; ++k)
+                for (int j = 0; j < CLASSES; ++j)
                 {
-                    if (schools[i].classes[j].students[k].getNationalId() == id)
+                    if (schools[i].classes[j].teacher.getNationalId() == id)
                     {
                         cout << schools[i].getName() << endl;
-                        cout << schools[i].classes[j].teacher.getFirstName() << " " << schools[i].classes[j].teacher.getLastName() << endl;
                     }
                 }
             }
         }
-    }
-    else if (choice == 7)
-    {
-        int id = 0;
-        cout << "enter the id of the school\n";
-        cin >> id;
-        for (int i = 0; i < schoolsQuantity; ++i)
+        else if (choice == 6)
         {
-            if (schools[i].getIdNumber() == id)
+            int id = 0;
+            cout << "enter the national id of the student\n";
+            cin >> id;
+            for (int i = 0; i < schoolsQuantity; ++i)
             {
-                for (int j = 0; j < 10; ++j)
+                for (int j = 0; j < CLASSES; ++j)
                 {
-                    cout << schools[i].classes[j].teacher.getFirstName() << " " << schools[i].classes[j].teacher.getLastName() << endl;
-                    cout << schools[i].classes[j].teacher.getNationalId() << endl;
+                    for (int k = 0; k < quantitySTUDENTS; ++k)
+                    {
+                        if (schools[i].classes[j].students[k].getNationalId() == id)
+                        {
+                            cout << schools[i].getName() << endl;
+                            cout << schools[i].classes[j].teacher.getFirstName() << " " << schools[i].classes[j].teacher.getLastName() << endl;
+                        }
+                    }
                 }
             }
         }
-    }
-    else if (choice == 8)
-    {
-        for (int i = 0; i < schoolsQuantity; ++i)
+        else if (choice == 7)
         {
+            int id = 0;
+            cout << "enter the id of the school\n";
+            cin >> id;
+            for (int i = 0; i < schoolsQuantity; ++i)
+            {
+                if (schools[i].getIdNumber() == id)
+                {
+                    for (int j = 0; j < CLASSES; ++j)
+                    {
+                        cout << schools[i].classes[j].teacher.getFirstName() << " " << schools[i].classes[j].teacher.getLastName() << endl;
+                        cout << schools[i].classes[j].teacher.getNationalId() << endl;
+                    }
+                }
+            }
+        }
+        else if (choice == 8)
+        {
+            for (int i = 0; i < schoolsQuantity; ++i)
+            {
+                for (int j = 0; j < CLASSES; ++j)
+                {
+                    for (int k = 0; k < quantitySTUDENTS; ++k)
+                    {
+                        cout << schools[i].classes[j].students[k].getFirstName() << " " << schools[i].classes[j].students[k].getLastName() << endl;
+                        cout << schools[i].classes[j].students[k].getNationalId() << endl;
+                    }
+                }
+            }
+        }
+        else if (choice == 9)
+        {
+            int day = 0;
+            int month = 0;
+            int year = 0;
+            cout << "enter the year then the month then the day\n";
+            cin >> year >> month >> day;
+            for (int i = 0; i < schoolsQuantity; ++i)
+            {
+                for (int j = 0; j < CLASSES; ++j)
+                {
+                    for (int k = 0; k < quantitySTUDENTS; ++k)
+                    {
+                        if (schools[i].classes[j].students[k].getBirthDay() == day && schools[i].classes[j].students[k].getBirthMonth() == month && schools[i].classes[j].students[k].getBirthYear() == year)
+                        {
+                            cout << schools[i].classes[j].students[k].getFirstName() << " " << schools[i].classes[j].students[k].getLastName() << endl;
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            cout << "have a nice time\n";
+            break;
+        }
+    }
 
-            for (int j = 0; j < 10; ++j)
-            {
-                for (int k = 0; k < 30; ++k)
-                {
-                    cout << schools[i].classes[j].students[k].getFirstName() << " " << schools[i].classes[j].students[k].getLastName() << endl;
-                    cout << schools[i].classes[j].students[k].getNationalId() << endl;
-                }
-            }
-        }
-    }
 }
 
 int length(int id)
@@ -441,22 +474,22 @@ bool nameValidationCheck(string firstName,string lastName)
     bool isLastNameValid = true;
     for (int i = 0; i < firstName.length(); ++i)
     {
-        if (!((firstName[i] >= 65 && firstName[i] <= 90) && (firstName[i] >= 97 && firstName[i] <= 122)))
+        if (!((firstName[i] >= 65 && firstName[i] <= 90) || (firstName[i] >= 97 && firstName[i] <= 122)))
         {
             isFirstNameValid = false;
         }
     }
     for (int i = 0; i < lastName.length(); ++i)
     {
-        if (!((lastName[i] >= 65 && lastName[i] <= 90) && (lastName[i] >= 97 && lastName[i] <= 122)))
+        if (!((lastName[i] >= 65 && lastName[i] <= 90) || (lastName[i] >= 97 && lastName[i] <= 122)))
         {
-            isFirstNameValid = false;
+            isLastNameValid = false;
         }
     }
     return isFirstNameValid && isLastNameValid;
 }
 
-Student setStudentInformation()
+ Student setStudentInformation()
 {
     Student student;
     string tempstr;
@@ -548,7 +581,7 @@ SchoolClass setClassesInformation()
     Student students;
     Teacher teacher = setTeacherInformation();
     schoolClass.teacher = teacher;
-    for (int i = 0; i < 30; ++i)
+    for (int i = 0; i < quantitySTUDENTS; ++i)
     {
         cout << "student number " << i + 1 << endl;
         students = setStudentInformation();
@@ -562,14 +595,14 @@ School setSchoolInformation()
     School school;
     string tempstr;
     int tempnum;
-    cout << "enter the name of the shool" << endl;
+    cout << "enter the name of the school" << endl;
     cin >> tempstr;
     school.setName(tempstr);
-    cout << "enter the ID of the shool" << endl;
+    cout << "enter the ID of the school" << endl;
     cin >> tempnum;
     school.setIdNumber(tempnum);
     SchoolClass schoolClasses;
-    for (int j = 0; j < 10; ++j)
+    for (int j = 0; j < CLASSES; ++j)
     {
         cout << "class number " << j + 1 << endl << endl;
         schoolClasses = setClassesInformation();
